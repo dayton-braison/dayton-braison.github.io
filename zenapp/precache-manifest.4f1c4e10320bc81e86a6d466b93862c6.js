ccdca7f139d1c5754b17cea65b739d6fdd16201c
Dayton Braison <daytonbraison@gmail.com>
2020-12-03 19:25:58 -0600

    Added initial version of zenapp

---
+++ zenapp/precache-manifest.4f1c4e10320bc81e86a6d466b93862c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24cc389f9ff7d096efc78e23dc37c1e9",
+    "url": "/zenapp/index.html"
+  },
+  {
+    "revision": "c95db2c5db8428a0d640",
+    "url": "/zenapp/static/css/main.713e00be.chunk.css"
+  },
+  {
+    "revision": "8cce67768349367a5038",
+    "url": "/zenapp/static/js/2.3e217575.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/zenapp/static/js/2.3e217575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c95db2c5db8428a0d640",
+    "url": "/zenapp/static/js/main.49ac28d3.chunk.js"
+  },
+  {
+    "revision": "291e6812b76e42e8dbbd",
+    "url": "/zenapp/static/js/runtime-main.2379a1d6.js"
+  }
+]);
